b6f206fab6eac1aa51687a41773d3c9262eb2e62
b-s-x <b.s.x.mars@gmail.com>
2021-01-25 12:29:49 +0300

    added pug and route

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require("express")
 const app = express()
+const path = require("path")
 
 const port = process.env.PORT || "8000"
+
+app.set("view engine", "pug");
+
+app.get("*", (req, res) => {
+  res.render("index")
+})
 
 app.listen(port, () => {
   console.log(`Server working on http://localhost:${port}` );

8746b5fda86a3d6ed5f78d995017d802465d3994
b-s-x <b.s.x.mars@gmail.com>
2021-01-25 15:20:48 +0300

    small fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,10 @@
 import Vue from 'vue'
 import App from './components/App'
 
-// import router from '@router/router';
-// import store from '@store/store';
+console.log(333);
 console.log("its working ffafsnfjj");
-// Vue.prototype.$eventBus = new Vue();
 
 const app = new Vue({
-  // router,
-  // store,
   el: '#app',
   render: h => h(App)
 });

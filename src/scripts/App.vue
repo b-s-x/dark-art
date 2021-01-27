115ebab2f17926702aa657b38bb0a7ecc2371225
b-s-x <b.s.x.mars@gmail.com>
2021-01-27 17:35:32 +0300

    added vue-router

--- src/scripts/App.vue
+++ src/scripts/App.vue
@@ -1,38 +1,15 @@
 <template lang="pug">
   div.main
-    div.ff VUE
-    div.ff VUE VUE
-    div.ff VUE VUE VUE
-    div.ff VUE VUE VUE VUE
-    div.ff VUE VUE VUE VUE VUE
-    div.ff VUE VUE VUE VUE
-    div.ff VUE VUE VUE
-    div.ff VUE VUE
-    div.ff VUE
+    router-view/
 </template>
 
 <script>
-import NewApp from "./components/NewApp"
 export default {
-  components: {
-    NewApp,
-  }
 }
 
 </script>
 
 <style lang="scss" scoped>
-$color: red;
-
-.main {
-  box-sizing: border-box;
-}
-
-.ff {
-  background: $color;
-  color: white;
-  font-size: 40px;
-}
 
 </style>
 

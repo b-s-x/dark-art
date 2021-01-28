e05323e3bc7b27ba2d330e424210abf9ea167082
Renat Tuktarov <yandzeek@gmail.com>
2021-01-28 20:18:10 +0300

    fix: working rendering + vue-router

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const { devMiddleware, hotMiddleware } = require('./server/hot-module-reload');
 const app = express()
+const fs = require('fs');
 
 const port = process.env.PORT || '8000'
 
@@ -12,6 +13,13 @@ app.use(devMiddleware, hotMiddleware)
 
 app.use(express.static(`${__dirname}/static`))
 app.use(express.static('static/dist'))
+
+app.use('*', (req, res) => {
+  const indexPath = path.resolve(__dirname, './static/dist/index.html');
+  const fileContent = fs.readFileSync(indexPath)
+
+  res.send(fileContent.toString()).end();
+});
 
 app.listen(port, () => {
   console.log(`Server working on http://localhost:${port}` );
